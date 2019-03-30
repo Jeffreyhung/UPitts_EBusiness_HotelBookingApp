b1017438b76b1338f4cca5a4f50862ef9a35f598
Jeffrey <jeffreyhung@hotmail.com.tw>
2019-03-30 17:07:50 -0400

    complete adding data to DB

--- src/com/hotel/BookingManage.java
+++ src/com/hotel/BookingManage.java
@@ -9,18 +9,10 @@ public class BookingManage {
 	private int rid;
 	private String startDate;
 	private String endDate;
-	private BookingInfo bookingInfo;
 	private Room room;
 	
 	public BookingManage() {}
 	
-	public BookingInfo getBookingInfo() {
-		return bookingInfo;
-	}
-	
-	public void setBookingInfo(BookingInfo bookingInfo) {
-		this.bookingInfo = bookingInfo;
-	}
 	
 	public void setRoom(Room room) {
 		this.room = room;
@@ -62,20 +54,7 @@ public class BookingManage {
 	}
 	
 	public void onload()  throws ClassNotFoundException, SQLException {
-		Connection connect = null;
-		
-		String url = "jdbc:mysql://localhost:3306/hotel";
-		
-		String username = "root";
-		String password = "root";
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connect = DriverManager.getConnection(url, username, password);
-		} catch (SQLException ex) {
-			System.out.println("in exec");
-			System.out.println(ex.getMessage());
-		}
+		Connection connect = connectDB();
 		PreparedStatement pstmt = connect.prepareStatement("SELECT RID, Name, Capacity, NumOfBeds, Price FROM room WHERE RID = ?");
 		pstmt.setInt(1, getRid());
 		ResultSet rs = pstmt.executeQuery();
@@ -93,12 +72,70 @@ public class BookingManage {
 		connect.close();
 	}
 	
-	public String addBooking(Customer cust) {
+	public String addBooking(Customer cust) throws ClassNotFoundException, SQLException{
 		Customer customer = cust;
+		int customerID = 0;
+		int bookingID = 0;
+		Connection connect = connectDB();
+		
+		// Get customer ID
+		PreparedStatement pstmt = connect.prepareStatement("SELECT MAX(cid) FROM customer");
+		ResultSet rs = pstmt.executeQuery();
+		while (rs.next()) {
+			customerID = rs.getInt("MAX(cid)")+1;
+		}
+		rs.close();
+		pstmt.close();
+		customer.setCid(customerID);
+		// Add value to customer table
+		PreparedStatement pstmt2 = connect.prepareStatement("INSERT INTO customer VALUES (?, ?, ?, ?, ?, ?, ?) ");
+		pstmt2.setInt(1, customer.getCid());
+		pstmt2.setString(2, customer.getBookingName());
+		pstmt2.setString(3, customer.getFirstName());
+		pstmt2.setString(4, customer.getLastName());
+		pstmt2.setString(5, customer.getAddress());
+		pstmt2.setString(6, Integer.toString(customer.getPhone()));
+		pstmt2.setString(7, customer.getEmail());
+		pstmt2.executeUpdate();
+		pstmt2.close();
+		// Get customer ID
+		PreparedStatement pstmt4 = connect.prepareStatement("SELECT MAX(bid) FROM bookinginfo");
+		ResultSet rs2 = pstmt4.executeQuery();
+		while (rs2.next()) {
+			bookingID = rs2.getInt("MAX(bid)")+1;
+		}
+		rs.close();
+		pstmt4.close();
+		// Add value to bookingInfo table
+		PreparedStatement pstmt3 = connect.prepareStatement("INSERT INTO bookinginfo VALUES (?, ?, ?, ?, ?) ");
+		pstmt3.setInt(1, bookingID);
+		pstmt3.setInt(2, customer.getCid());
+		pstmt3.setInt(3, rid);
+		pstmt3.setString(4, startDate);
+		pstmt3.setString(5, endDate);
+		pstmt3.executeUpdate();
+		pstmt3.close();
+		
+		connect.close();
 		
 		
 //		return "confirmation?faces-redirect=true";
 		return "True";
+	}
+	
+	private Connection connectDB()  throws ClassNotFoundException{
+		Connection connect = null;
+		String url = "jdbc:mysql://localhost:3306/hotel";
+		String username = "root";
+		String password = "root";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connect = DriverManager.getConnection(url, username, password);
+		} catch (SQLException ex) {
+			System.out.println("in exec");
+			System.out.println(ex.getMessage());
+		}
+		return connect;
 	}
 	
 }
